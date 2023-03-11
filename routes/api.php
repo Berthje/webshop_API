55ab4fe37387f3605e6c1e2afba82430e7d27e74
layton <layton.berth@student.howest.be>
2023-03-11 17:53:12 +0100

    added function to get a product by id only

--- routes/api.php
+++ routes/api.php
@@ -5,5 +5,5 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 
 
-
 Route::get('/products', [ProductController::class, "allProducts"]);
+Route::get('/products/{id}', [ProductController::class, "productById"]);
